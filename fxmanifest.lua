38f835b4506980fd0a9901b4dc3727d30cb5a428
GamingLuke1337 <64887613+GamingLuke1337@users.noreply.github.com>
2025-02-24 04:58:12 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,6 +3,11 @@ game 'gta5'
 
 lua54 'yes'
 
+author 'Amazing people on the internet'
+description 'Emotes & Animations for Your FiveM Server'
+version '1.0.0'
+repository 'https://github.com/GamingLuke1337/dpemotes'
+
 client_scripts {
 	'NativeUI.lua',
 	'client/*.lua'
